d9abe2c4bf9b8f3bfb5edd2ad71cbfdcf315e4ce
Elena Beckers <46313514+elenabeckers@users.noreply.github.com>
2025-01-25 17:44:23 -0700

    fix: remove usless span from skeleton

--- src/components/Product/Search/List/Skeleton.tsx
+++ src/components/Product/Search/List/Skeleton.tsx
@@ -33,7 +33,6 @@ class ProductSearchGridSkeleton extends React.Component<ProductSearchGridSkeleto
             <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5"></div>
             <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
           </div>
-          <span className="sr-only">Loading...</span>
         </li>
       )
     );
