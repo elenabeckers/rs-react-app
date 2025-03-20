2baf616d2e099eb5395ecc59570b5df5cef4c8fe
Elena Beckers <46313514+elenabeckers@users.noreply.github.com>
2025-03-20 09:49:41 -0600

    feat: add country list item component

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,4 +1,5 @@
 import { Country } from '../types';
+import CountryListItem from './CountryListItem';
 import Loader from './Loader';
 
 interface CountryListProps {
@@ -18,36 +19,17 @@ const CountryList = ({
 
   return (
     <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 p-4 max-w-6xl mx-auto">
-      {countries?.map(
-        ({ name: { common }, population, region, flags: { png, alt } }) => {
-          const isVisited = visitedCountries?.has(common);
-
-          return (
-            <li
-              key={common}
-              className={`${isVisited ? 'bg-green-300' : 'bg-white'} shadow-md rounded-2xl overflow-hidden transform transition duration-300 hover:scale-105 w-full max-w-xs mx-auto cursor-pointer`}
-              onClick={() => onClick(common)}
-            >
-              <img src={png} alt={alt} className="w-full h-20 object-cover" />
-              <div className="p-4">
-                <h2 className="text-lg font-bold text-gray-90 mb-2">
-                  {common}
-                </h2>
-                <p className="text-gray-700 text-sm">
-                  <span className="font-semibold">Population:</span>{' '}
-                  {population.toLocaleString()}
-                </p>
-                <p className="text-gray-700 text-sm">
-                  <span className="font-semibold">Region:</span> {region}
-                </p>
-                <p className="text-gray-700 text-sm h-6">
-                  {isVisited && 'Country was visited!'}
-                </p>
-              </div>
-            </li>
-          );
-        }
-      )}
+      {countries?.map((country) => {
+        const isVisited = Boolean(visitedCountries?.has(country.name.common));
+        return (
+          <CountryListItem
+            key={country.name.common}
+            country={country}
+            isVisited={isVisited}
+            onClick={onClick}
+          />
+        );
+      })}
     </ul>
   );
 };
